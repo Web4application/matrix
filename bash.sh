71d5fb2ac2217bb88b652e09564cd5ba51b40a13
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-22 22:09:31 +0100

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,15 @@
+```
+/assets/
+    paperweb.css
+    paperweb-dark.css
+    matrix-sim.js
+    matrix-grid.js
+
+/includes/
+    atlas-cover.html
+    paperweb-nav.html
+    human-matrix-atlas.html
+
+/pages/
+    atlas.html
+    atlas-pdf.html
